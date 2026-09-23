36d3dc3fb74822cb52242db7ac3a091533623ef8
Natan França <natan.j.franca@gmail.com>
2026-09-22 21:57:48 -0300

    test: criação de test para validação

---
+++ tests/parser/issue_1/valido_atribuicoes.java
@@ -0,0 +1,13 @@
+public class valido_atribuicoes {
+    void executar() {
+        int x, contador, saldo, y;
+
+        x = 5;
+        contador += 1;
+        saldo -= 10;
+        x++;
+        y--;
+        ++x;
+        --y;
+    }
+}
---
+++ tests/parser/issue_1/valido_inicializacoes.java
@@ -0,0 +1,5 @@
+public class valido_inicializacoes {
+    int contador = 42;
+    double preco = 10;
+    final int LIMITE = 100;
+}
